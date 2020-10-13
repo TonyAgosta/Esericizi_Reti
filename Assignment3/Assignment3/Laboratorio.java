464a1070ab543b34a73c4f290edf053cf5589aad
Tony Agosta <tagosta97@gmail.com>
2020-10-13 08:15:25 -0700

    Add files via upload

---
+++ Assignment3/Assignment3/Laboratorio.java
@@ -0,0 +1,85 @@
+//Tony Agosta 544090
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.*;
+
+public class Laboratorio {
+    final Lock lockcoda;
+    final Condition pcFree;
+    final Condition labFull;
+    final Condition tesistaFree;
+    ArrayList<String> computer;
+
+    // costruttore
+    public Laboratorio() {
+        this.computer = new ArrayList<>(20);
+        for (int i = 0; i < 20; i++) {
+            computer.add(i, "free");
+        }
+        lockcoda = new ReentrantLock();
+        pcFree = lockcoda.newCondition();
+        labFull = lockcoda.newCondition();
+        tesistaFree = lockcoda.newCondition();
+    }
+
+    // metodo che controlla se l'intero laboratorio e` occupato o meno
+    // e restitusce true se trova tutti i computer liberi, false se ne trova
+    // almeno uno occupato
+    public boolean interolaboratoriolibero() {
+        for (int i = 0; i < 20; i++) {
+            if (computer.get(i).equals("notfree"))
+                return false;
+        }
+        return true;
+    }
+
+    // metodo che restituisce il numero di computer liberi
+    public int computerliberi() {
+        int numeropcliberi = 0;// variabile in cui salvo il numero totale di pc liberi
+        for (int i = 0; i < 20; i++)
+            if (computer.get(i).equals("free"))
+                ;
+        numeropcliberi++;
+        return numeropcliberi;
+    }
+
+    // controllo che il computer richisto dal tesista sia libero
+    public boolean computertesista(int index) {
+        if (computer.get(index).equals("free"))
+            return true;
+        else
+            return false;
+    }
+
+    // restituisco l'indice del primo pc libero
+    public int computerlibero() {
+        for (int i = 0; i < 20; i++) {
+            if (computer.get(i).equals("free"))
+                return i;
+        }
+        return 404; // da sistemare
+    }
+
+    // set di tutti i computer del laboratorio a notfree
+    public void occupacomputer() {
+        for (int i = 0; i < 20; i++)
+            computer.set(i, "notfree");
+    }
+
+    // set del computer ,identificato dall'indice passato come argomento, a notfree
+    public void occupasingolopc(int indicepcoccupato) {
+        computer.set(indicepcoccupato, "notfree");
+    }
+
+    // set del computer,identificato dall'indice passato come argomento, a free
+    public void liberapc(int pcdaliberare) {
+        computer.set(pcdaliberare, "free");
+    }
+
+    // set di tutti i computer a free
+    public void liberalaboratorio() {
+        for (int i = 0; i < 20; i++) {
+            computer.set(i, "free");
+        }
+    }
+}
---
+++ Assignment3/Assignment3/Utenti.java
@@ -0,0 +1,125 @@
+//Tony Agosta 544090
+
+public class Utenti extends Thread {
+    private int priority;
+    private int access;
+    private int indextesista; // indice del computer usato dai tesisti
+    private Laboratorio lab;
+
+    // costruttore
+    public Utenti(int priority, int access, int indextesista, Laboratorio lab) {
+        this.priority = priority;
+        this.access = access; // numero degli accessi al laboratorio che fa ogni utente
+        this.indextesista = indextesista;
+        this.lab = lab;
+    }
+
+    public void run() {
+
+        int j;
+        for (int i = 0; i < access; i++) {
+
+            // studente
+            if (this.priority == 1) {
+                lab.lockcoda.lock();
+                while (lab.computerliberi() == 0) {
+                    try {
+                        lab.pcFree.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                j = lab.computerlibero();// indice del primo computer constrassegnato come free
+                while (j == 404) {
+                    j = lab.computerlibero();
+                }
+                System.out.printf("Lo studente %s ha occupato il computer %d\n", Thread.currentThread().getName(), j);
+
+                lab.occupasingolopc(j); // set del pc appeno occupato a notfree
+                lab.lockcoda.unlock();
+                try {
+                    Thread.sleep((long) Math.random() * 5000); // uso la sleep per simulare l'utilizzo del computer
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.printf("Lo studente %s ha liberato il computer %d\n", Thread.currentThread().getName(), j);
+                lab.lockcoda.lock();
+                lab.liberapc(j);// set a free del computer dopo aver finito di utilizzarlo
+                lab.labFull.signal();
+                lab.tesistaFree.signal();
+                lab.pcFree.signal();
+
+                lab.lockcoda.unlock();
+            }
+            // tesista
+            else if (this.priority == 2) {
+                lab.lockcoda.lock();
+                while (!lab.computertesista(indextesista)) {
+                    try {
+                        lab.tesistaFree.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.printf("Il tesista %s ha occupato il computer numero %d\n", Thread.currentThread().getName(),
+                        indextesista);
+
+                lab.occupasingolopc(indextesista); // set del pc richiesto dal tesista a notfree
+                lab.lockcoda.unlock();
+                try {
+                    Thread.sleep((long) Math.random() * 5000); // uso la sleep per simulare l'utilizzo del computer
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.printf("Il tesista %s ha liberato il computer numero %d\n", Thread.currentThread().getName(),
+                        indextesista);
+                lab.lockcoda.lock();
+                lab.liberapc(indextesista);// set a free del computer dopo aver finito di utilizzarlo
+
+                lab.labFull.signal();
+                lab.tesistaFree.signal();
+                lab.pcFree.signal();
+                lab.lockcoda.unlock();
+
+            }
+
+            // professore
+            else if (this.priority == 3) {
+                lab.lockcoda.lock();
+                while (!lab.interolaboratoriolibero()) {
+                    try {
+                        lab.labFull.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.printf("Il professore %s ha occupato tutto il laboratorio\n",
+                        Thread.currentThread().getName());
+                lab.occupacomputer();// set di tutti i computer a notfree
+                lab.lockcoda.unlock();
+
+                try {
+                    Thread.sleep((long) Math.random() * 6000);// sleep per simulare l'utilizzo del computer
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.printf("Il professore %s ha liberato tutto il laboratorio\n",
+                        Thread.currentThread().getName());
+                lab.lockcoda.lock();
+                lab.liberalaboratorio();
+                lab.labFull.signalAll();
+                lab.tesistaFree.signal();
+                lab.pcFree.signal();
+                lab.lockcoda.unlock();
+            }
+
+            try {
+                Thread.sleep((long) (Math.random() * 15000));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
