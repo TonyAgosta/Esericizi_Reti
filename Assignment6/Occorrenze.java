e884435cbe5b9db93063b2b0f714301928b03136
Tony Agosta <tagosta97@gmail.com>
2020-11-08 07:47:09 -0800

    Add files via upload

---
+++ Assignment6/Occorrenze.java
@@ -0,0 +1,61 @@
+//Tony Agosta 544090
+
+//La classe occorrenze continene i metodi usati per incrementare in modo thread-safe i valori delle occorrenze dei movimenti; 
+//inoltre contiene anche i metodi che vegono utilzzati per restituire il totale delle occorrenze di ogni tipo di causale
+
+public class Occorrenze {
+    private int Bollettino;
+    private int F24;
+    private int PagoBancomat;
+    private int Accredito;
+    private int Bonifico;
+
+    public Occorrenze() {
+        Bollettino = 0;
+        F24 = 0;
+        PagoBancomat = 0;
+        Accredito = 0;
+        Bonifico = 0;
+    }
+
+    public synchronized void addBonifico() {
+        this.Bonifico++;
+    }
+
+    public synchronized void addBollettino() {
+        this.Bollettino++;
+    }
+
+    public synchronized void addF24() {
+        this.F24++;
+    }
+
+    public synchronized void addAccredito() {
+        this.Accredito++;
+    }
+
+    public synchronized void addPagoBancomato() {
+        this.PagoBancomat++;
+    }
+
+    public int getBonifico() {
+        return this.Bonifico;
+    }
+
+    public int getBollettino() {
+        return this.Bollettino;
+    }
+
+    public int getF24() {
+        return this.F24;
+    }
+
+    public int getAccredito() {
+        return this.Accredito;
+    }
+
+    public int getPagoBancomat() {
+        return this.PagoBancomat;
+    }
+
+}
